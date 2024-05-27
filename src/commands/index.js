1df833db7e5a790280bb4baa2842dc472d7003f5
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-05-27 18:05:26 +0530

    Added: `src/commands/index.js`

---
+++ src/commands/index.js
@@ -0,0 +1,11 @@
+// File: src/commands/index.js (JavaScript)
+
+const moderation = require('./moderation');
+const custom = require('./custom');
+const settings = require('./settings');
+
+module.exports = {
+  moderation,
+  custom,
+  settings,
+};
